4af460106493536e08ae451db7e5271e6bea0ca9
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:04:33 +0500

    Update init.php

--- init.php
+++ init.php
@@ -2,6 +2,5 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-chartjs')
     ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
-    ->ngModule('chart.js')
     ->jsPackage('angular-chart.min.js')
 ;
